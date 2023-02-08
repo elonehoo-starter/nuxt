0fbb056a9c9b96fdfc44c1240f755b7388c63872
elonehoo <hi@elonehoo.me>
2023-02-08 12:41:35 +0800

    style: run eslint fix

--- app.vue
+++ app.vue
@@ -12,7 +12,7 @@ useHead({
 <template>
   <NuxtLayout>
     <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-      <NuxtPage/>
+      <NuxtPage />
       <LazyFooter />
     </main>
   </NuxtLayout>
