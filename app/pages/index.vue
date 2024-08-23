6a2e6ff7de5506811ae0dccf3d0c45e0b60b5b54
elonehoo <hi@elonehoo.me>
2024-08-23 21:32:13 +0800

    fix: update nuxt 4

---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div i-carbon-piggy-bank-slot text-4xl inline-block />
+    <Suspense>
+      <LazyViewsPageView />
+      <template #fallback>
+        <div op50 italic>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <LazyInputEntry />
+  </div>
+</template>
